a5de8670115b76602f1dcadb76bb26d5a5b2b2d9
NatalyaOshmarina <91863446+NatalyaOshmarina@users.noreply.github.com>
2025-05-12 09:52:18 +0500

    добавлен абсолютный путь к папке data

--- load.py
+++ load.py
@@ -10,9 +10,11 @@ dirname = os.path.dirname(__file__)
 config = configparser.ConfigParser()
 config.read(os.path.join(dirname, 'config.ini'), encoding='utf-8')
 path = config['Paths']['SALES_PATH']
+SALES_PATH = os.path.join(dirname, raw_path) # определяем абсолютный путь
+
 database = Database()
 
-files = glob(path + '/*.csv')
+files = glob(SALES_PATH + '/*.csv')
 for file in files:
     sales_df = pd.read_csv(file)
     database.post(sales_df)
